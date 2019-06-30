36dea2c6b8828bfaab9d383a3e41804b6919086e
ken <kenokech94@gmail.com>
2019-06-30 17:38:59 +0200

    fixed log entry

--- torrent_tracker_scraper/scraper.py
+++ torrent_tracker_scraper/scraper.py
@@ -105,7 +105,7 @@ class Scraper:
 
                 index = 8
                 for i in range(1, len(infohashes) + 1):
-                    MyLogger.log("Offset: {} {}".format(index, index + (i * 12)), logging.INFO)
+                    MyLogger.log("Offset: {} {}".format(index + (i * 12) - 12, index + (i * 12)), logging.INFO)
                     seeders, completed, leechers = struct.unpack(">LLL", res[index + (i * 12) - 12: index + (i * 12)])
                     MyLogger.log("{} {} {}".format(seeders, completed, seeders), logging.INFO)
 
