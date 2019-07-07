b1d15a18af8694970e0899abb06406eab5b41dbb
ken <kenokech94@gmail.com>
2019-07-07 08:39:15 +0200

    refactored tests

--- tests/scraper_test.py
+++ tests/scraper_test.py
@@ -2,7 +2,7 @@
 import os
 import unittest
 
-from torrent_tracker_scraper import scraper
+from scraper import Scraper
 
 GOOD_INFOHASHES_PATH = os.path.join(os.path.dirname(__file__), 'good_infohashes.txt')
 BAD_INFOHASHES_PATH = os.path.join(os.path.dirname(__file__), 'bad_infohashes.txt')
@@ -10,7 +10,7 @@ BAD_INFOHASHES_PATH = os.path.join(os.path.dirname(__file__), 'bad_infohashes.tx
 
 class ScrapeTest(unittest.TestCase):
 
-    def test_scraper(self):
+    def test_scraper_with_single_infohash(self):
         f = open(GOOD_INFOHASHES_PATH, 'rb')
 
         content = f.readlines()
@@ -18,16 +18,13 @@ class ScrapeTest(unittest.TestCase):
         for infohash in content:
             infohash = infohash.decode('utf-8')
             infohash = infohash.strip()
-            torrent_infohash, seeders, leechers, complete = scraper.scrape(
-                infohash,
-                "tracker.coppersurfer.tk",
-                6969
-            )
-            self.assertEqual(torrent_infohash, infohash)
+            _scraper = Scraper("tracker.coppersuffer.tk", 6969, json=True)
+            results = _scraper.scrape(infohash)
+            self.assertEqual(results.get('results')[0].get('infohash'), infohash)
 
         f.close()
 
-    def test_scraper_infohash_error(self):
+    def test_scraper_with_single_bad_infohash(self):
         f = open(BAD_INFOHASHES_PATH, 'rb')
 
         content = f.readlines()
@@ -35,10 +32,9 @@ class ScrapeTest(unittest.TestCase):
         for infohash in content:
             infohash = infohash.decode('utf-8')
             infohash = infohash.strip()
-            error = scraper.scrape(infohash,
-                                   "tracker.coppersurfer.tk",
-                                   6969)
-            self.assertEqual(error, "Invalid infohash {0}".format(infohash))
+            _scraper = Scraper("tracker.coppersuffer.tk", 6969, json=True)
+            results = _scraper.scrape(infohash)
+            self.assertEqual(results, "Invalid infohash {0}, skipping".format(infohash))
 
         f.close()
 
@@ -50,11 +46,9 @@ class ScrapeTest(unittest.TestCase):
         for infohash in content:
             infohash = infohash.decode('utf-8')
             infohash = infohash.strip()
-            error = scraper.scrape(
-                infohash,
-                "invalidhostname.tk",
-                6969
-            )
+            _scraper = Scraper("invalidhostname.tk", 6969, json=True)
+            error = _scraper.scrape(infohash)
+            breakpoint()
             self.assertEqual(tuple, type(error))
 
         f.close()
