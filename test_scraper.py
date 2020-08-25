1c24c75476d5e03386292a2dcf7a3d75fe72bbf9
azerty <kenokech94@gmail.com>
2020-08-25 19:13:08 +0200

    Fix tests, black formatting

--- test_scraper.py
+++ test_scraper.py
@@ -1,11 +1,11 @@
 from socket import socket
 
 import pytest
-
 from torrent_tracker_scraper import scraper
 from torrent_tracker_scraper.scraper import Connection
 
 # TODO: Improve tests: fixtures, mocks
+
 
 @pytest.fixture
 def scraper_basic():
@@ -21,9 +21,12 @@ def scraper_basic():
     # Use together
     scraper_.connect_response = b"\x00\x00\x00\x00\x00\x008@P\x87\xe0m\x108\xf6r"
     scraper_.transaction_id = 14400  # fixed in return value of socket.recv
-    scraper_.connection_id = 5802853403918399090  #  fixed in return value of socket.recv
+    scraper_.connection_id = (
+        5802853403918399090  #  fixed in return value of socket.recv
+    )
 
     return scraper_
+
 
 @pytest.fixture
 def mock_response():
@@ -62,45 +65,45 @@ def mixed_infohashes():
     ]
 
 
-def test_parse_none_infohashes():
+def test_get_good_infohashes():
     scraper_ = scraper.Scraper()
-    assert scraper_.parse_infohashes() == []
+    assert scraper_.get_good_infohashes() == []
 
 
-def test_parse_empty_infohashes():
+def test_get_good_infohashes_empty_list():
     scraper_ = scraper.Scraper(infohashes=[])
-    assert scraper_.parse_infohashes() == []
+    assert scraper_.get_good_infohashes() == []
 
 
-def test_single_infohash_parsing(good_infohashes):
+def test_get_good_infohashes_single_infohash(good_infohashes):
     scraper_ = scraper.Scraper(infohashes=good_infohashes[0])
-    assert scraper_.parse_infohashes() == [good_infohashes[0]]
+    assert scraper_.get_good_infohashes() == [good_infohashes[0]]
 
 
-def test_multiple_infohashes_parsing(good_infohashes):
+def test_get_good_infohashes_with_multiple_infohashes(good_infohashes):
     scraper_ = scraper.Scraper(infohashes=good_infohashes)
-    assert scraper_.parse_infohashes() == good_infohashes
+    assert scraper_.get_good_infohashes() == good_infohashes
 
 
-def test_string_type_infohashes_parsing(good_infohashes):
+def test_get_good_infohashes_string_type_infohashes(good_infohashes):
     scraper_ = scraper.Scraper(
         infohashes="88334ec1d90afe94a22c6de5756268599f5f8ea2,5b6a484a018beed4d01f2f57e6d029a4190a9d04"
     )
-    assert scraper_.parse_infohashes() == good_infohashes
+    assert scraper_.get_good_infohashes() == good_infohashes
 
 
-def test_string_type_infohashes_parsing_with_multiple_commas(good_infohashes):
+def test_get_good_infohashes_string_type_with_multiple_commas(good_infohashes):
     scraper_ = scraper.Scraper(
         infohashes="88334ec1d90afe94a22c6de5756268599f5f8ea2,5b6a484a018beed4d01f2f57e6d029a4190a9d04, , ,"
     )
-    assert scraper_.parse_infohashes() == good_infohashes
+    assert scraper_.get_good_infohashes() == good_infohashes
 
 
-def test_bad_infohash_parsing(mixed_infohashes):
+def test_get_good_infohashes_bad_infohash(mixed_infohashes):
     scraper_ = scraper.Scraper(infohashes=["88334ec1d90afe94a22c6de575626"])
-    assert scraper_.parse_infohashes() == []
+    assert scraper_.get_good_infohashes() == []
     scraper_ = scraper.Scraper(infohashes=mixed_infohashes)
-    assert scraper_.parse_infohashes() == [
+    assert scraper_.get_good_infohashes() == [
         "88334ec1d90afe94a22c6de5756268599f5f8ea2",
         "5b6a484a018beed4d01f2f57e6d029a4190a9d04",
     ]
@@ -113,9 +116,14 @@ def test_get_trackers_return_type(scraper_basic):
 def test_connect_request(monkeypatch, scraper_basic):
     recv = lambda s, f: scraper_basic.connect_response
     monkeypatch.setattr(socket, "recv", recv)
-    response_transaction_id, connection_id = scraper_basic._connect_request(scraper_basic.transaction_id)
+    response_transaction_id, connection_id = scraper_basic._connect_request(
+        scraper_basic.transaction_id
+    )
 
-    assert (response_transaction_id, connection_id) == (scraper_basic.transaction_id, scraper_basic.connection_id)
+    assert (response_transaction_id, connection_id) == (
+        scraper_basic.transaction_id,
+        scraper_basic.connection_id,
+    )
 
 
 def test_connect_request_failure(monkeypatch, scraper_basic):
