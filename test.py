912ba6e810ed79a549d5f80a63c491f8e2accc3a
ken <kenokech94@gmail.com>
2021-06-13 01:01:36 +0200

    refactor: Cleanup unneeded files

--- test.py
+++
@@ -1,8 +0,0 @@
-from torrent_tracker_scraper import scraper
-
-scraper = scraper.Scraper(
-    # trackers=["udp//:camera.lei001.com:6969/announce"],
-    infohashes=["FD55F8F6C08719D4D5E15EBF5EACEFBEC9ECE920",],
-)
-results = scraper.scrape()
-print(results)
